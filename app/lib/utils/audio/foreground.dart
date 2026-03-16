493e22b7e26d7be8ca1f3d170248291bc163cbc0
john-gitdev <john.wayne.lee@gmail.com>
2026-03-15 22:57:40 -0700

    chore: remove geolocator package and location code from foreground task

--- app/lib/utils/audio/foreground.dart
+++ app/lib/utils/audio/foreground.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
-import 'package:geolocator/geolocator.dart';
 
 import 'package:omi/utils/logger.dart';
 import 'package:omi/utils/platform/platform_service.dart';
@@ -12,50 +11,19 @@ void _startForegroundCallback() {
 }
 
 class _ForegroundFirstTaskHandler extends TaskHandler {
-  DateTime? _locationUpdatedAt;
-
   @override
   Future<void> onStart(DateTime timestamp, TaskStarter taskStarter) async {
     Logger.debug("Starting foreground task");
-    _locationInBackground();
-  }
-
-  Future _locationInBackground() async {
-    if (await Geolocator.isLocationServiceEnabled()) {
-      if (await Geolocator.checkPermission() == LocationPermission.always) {
-        var locationData = await Geolocator.getCurrentPosition();
-        if (_locationUpdatedAt == null ||
-            _locationUpdatedAt!.isBefore(DateTime.now().subtract(const Duration(minutes: 5)))) {
-          Object loc = {
-            "latitude": locationData.latitude,
-            "longitude": locationData.longitude,
-            'altitude': locationData.altitude,
-            'accuracy': locationData.accuracy,
-            'time': locationData.timestamp.toUtc().toIso8601String(),
-          };
-          FlutterForegroundTask.sendDataToMain(loc);
-          _locationUpdatedAt = DateTime.now();
-        }
-      } else {
-        Object loc = {'error': 'Always location permission is not granted'};
-        FlutterForegroundTask.sendDataToMain(loc);
-      }
-    } else {
-      Object loc = {'error': 'Location service is not enabled'};
-      FlutterForegroundTask.sendDataToMain(loc);
-    }
   }
 
   @override
   void onReceiveData(Object data) async {
     Logger.debug('onReceiveData: $data');
-    await _locationInBackground();
   }
 
   @override
   void onRepeatEvent(DateTime timestamp) async {
     Logger.debug("Foreground repeat event triggered");
-    await _locationInBackground();
   }
 
   @override
@@ -80,9 +48,6 @@ class ForegroundUtil {
     }
 
     if (Platform.isAndroid) {
-      // if (!await FlutterForegroundTask.canDrawOverlays) {
-      //   await FlutterForegroundTask.openSystemAlertWindowSettings();
-      // }
       if (!await FlutterForegroundTask.isIgnoringBatteryOptimizations) {
         await FlutterForegroundTask.requestIgnoreBatteryOptimization();
       }
@@ -114,19 +79,12 @@ class ForegroundUtil {
           channelDescription: 'Transcription service is running in the background.',
           channelImportance: NotificationChannelImportance.LOW,
           priority: NotificationPriority.HIGH,
-          // iconData: const NotificationIconData(
-          //   resType: ResourceType.mipmap,
-          //   resPrefix: ResourcePrefix.ic,
-          //   name: 'launcher',
-          // ),
         ),
         iosNotificationOptions: const IOSNotificationOptions(
           showNotification: false,
           playSound: false,
         ),
         foregroundTaskOptions: ForegroundTaskOptions(
-          // Warn: 5m, for location tracking. If we want to support other services, we use the differenct interval,
-          // such as 1m + self-validation in each service.
           eventAction: ForegroundTaskEventAction.repeat(60 * 1000 * 5),
           autoRunOnBoot: false,
           allowWakeLock: false,
