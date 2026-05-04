678e01f69bd4aa8c2aaac7436d8cbf89fde42952
john-gitdev <john.wayne.lee@gmail.com>
2026-05-03 17:17:50 -0700

    fix(ble): prevent redundant pairing popups by defaulting to non-bonding connections

--- app/lib/services/devices/device_connection.dart
+++ app/lib/services/devices/device_connection.dart
@@ -10,7 +10,7 @@ import 'package:omi/services/devices/transports/native_ble_transport.dart';
 import 'errors.dart';
 
 class DeviceConnectionFactory {
-  static DeviceConnection? create(BtDevice device, {bool requiresBond = true}) {
+  static DeviceConnection? create(BtDevice device, {bool requiresBond = false}) {
     DeviceTransport transport = NativeBleTransport(device.id, requiresBond: requiresBond);
     return OmiDeviceConnection(device, transport);
   }
