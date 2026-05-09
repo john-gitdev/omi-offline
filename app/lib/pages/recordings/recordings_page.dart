8efc92e1bef369eed32b7c00e60d936cb9bf06c5
john-gitdev <john.wayne.lee@gmail.com>
2026-05-08 19:34:25 -0700

    fix(ui): reduce left padding for status icons in AppBar

--- app/lib/pages/recordings/recordings_page.dart
+++ app/lib/pages/recordings/recordings_page.dart
@@ -510,7 +510,6 @@ class _RecordingsPageState extends State<RecordingsPage> {
               leading: !deviceProvider.isConnected
                   ? Container(
                       alignment: Alignment.centerLeft,
-                      padding: const EdgeInsets.only(left: 8.0),
                       child: deviceProvider.isConnecting
                           ? const Padding(
                               padding: EdgeInsets.only(left: 8.0),
