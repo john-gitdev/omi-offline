e5c43dcb485b984d1d1ef28266d67e5d061afbc4
john-gitdev <john.wayne.lee@gmail.com>
2026-06-08 14:13:02 -0700

    fix: build errors in find_devices_page.dart
    - Added status getter to IDeviceService interface.
    
    - Added missing import of devices.dart in find_devices_page.dart.

--- app/lib/pages/settings/find_devices_page.dart
+++ app/lib/pages/settings/find_devices_page.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:omi/backend/preferences.dart';
 import 'package:omi/backend/schema/bt_device/bt_device.dart';
 import 'package:omi/providers/device_provider.dart';
+import 'package:omi/services/devices.dart';
 import 'package:omi/services/services.dart';
 import 'package:omi/utils/logger.dart';
 import 'package:omi/gen/pigeon_communicator.g.dart';
